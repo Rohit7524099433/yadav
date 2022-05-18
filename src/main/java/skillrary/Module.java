5135bdecf9b78645746704a89d45778ba29b4fcb
Rohit7524099433 <rohitkumaryadav12.rky@gmail.com>
2022-05-18 18:09:44 +0530

    webhook

--- src/main/java/skillrary/Module.java
+++ src/main/java/skillrary/Module.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello why is it happening ?" );
+        System.out.println( "Hello hi" );
     }
 }
